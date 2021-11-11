efa9b675beca6027bcbd538534f9c964e6f9c0bf
Kateryna Tsygankova <kateryna.tsygankova@gmail.com>
2021-11-11 09:52:59 +0200

    task 9

--- js/task-06.js
+++ js/task-06.js
@@ -4,8 +4,8 @@ const refs = {
 
 refs.input.addEventListener('blur', onBlur)
 
-function onBlur(event){
-refs.input.classList.remove('valid', 'invalid')
+function onBlur(){
+refs.input.classList.remove('valid', 'invalid');
 refs.input.dataset.length == refs.input.value.length ?
 refs.input.classList.add('valid') :
 refs.input.classList.add('invalid');}
--- js/task-09.js
+++ js/task-09.js
@@ -1,16 +1,17 @@
-refs = {
+const refs = {
     buttonChangeColor: document.querySelector('.change-color'),
-    span: document.querySelector('.color')
+    span: document.querySelector('.color'),
+    body: document.querySelector('body')
 }
-
-function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-  }
 
 refs.buttonChangeColor.addEventListener('click', colorChange)
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  };
+
 function colorChange () {
-
+const randomColor = getRandomHexColor();
+refs.body.style.background = randomColor;
+refs.span.textContent = randomColor;
 }
-
-
